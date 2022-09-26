3fcc77ecb95bf894b069e73dbf9c8c8e0e167268
Brendan Ashworth <brendan.ashworth@me.com>
2022-09-25 22:44:02 -0700

    enable pulling layers from postgis

--- mundipy/layer.py
+++ mundipy/layer.py
@@ -1,37 +1,60 @@
 from shapely.geometry.base import BaseGeometry
 import geopandas as gpd
+from sqlalchemy import create_engine
 
 """A Layer represents a group of spatial data."""
 class Layer:
 
 	def __init__(self, data):
+		self.filename = None
+		self._dataframe = None
+		self._db_url = None
+		self._db_table = None
+
 		""" Initialize a Layer from a data source. """
 		if isinstance(data, gpd.GeoDataFrame):
-			self.filename = None
 			self._dataframe = data
+		elif isinstance(data, dict):
+			self._db_url = data['url']
+			self._db_table = data['table']
 		elif isinstance(data, str):
 			self.filename = data
-			self._dataframe = None
 		else:
-			raise TypeError('data for Layer() is neither filename nor GeoDataFrame')
+			raise TypeError('data for Layer() is not filename, GeoDataFrame or dict with PostgreSQL details')
+
+	def _load(self, bbox=None):
+		"""Load part or the entire Layer as a dataframe."""
+		if self._dataframe is not None:
+			return self._dataframe
+
+		if self._db_url is not None:
+			# load from PostGIS
+			con = create_engine(self._db_url)
+
+			# build the query
+			query = "SELECT * FROM %s" % self._db_table
+			if bbox is not None:
+				query = "SELECT * FROM %s WHERE geometry && ST_MakeEnvelope(%f, %f, %f, %f, 4326)" % ((self._db_table,) + bbox)
+
+			# assume it's WGS84
+			return gpd.GeoDataFrame.from_postgis(query, con, geom_col='geometry', crs='EPSG:4326')
+
+		if bbox is None:
+			return gpd.read_file(self.filename)
+		else:
+			return gpd.read_file(self.filename, bbox=bbox)
 
 	@property
 	def dataframe(self):
 		"""Load an entire Layer as a dataframe."""
-		if self._dataframe is not None:
-			return self._dataframe
-
-		return gpd.read_file(self.filename)
+		return self._load()
 
 	"""Read into a Layer at a specific geometry (WGS84)."""
 	def inside_bbox(self, bbox):
 		if not isinstance(bbox, tuple) or len(bbox) != 4:
 			raise TypeError('inside_bbox expected bbox to be a 4-tuple')
 
-		if self._dataframe is not None:
-			return self._dataframe
-		elif self.filename is not None:
-			return gpd.read_file(self.filename, bbox=bbox)
+		return self._load(bbox=bbox)
 
 
 """VisibleLayer represents Layer data, as seen from a geometry."""
