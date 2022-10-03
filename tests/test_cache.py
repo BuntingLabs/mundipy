11f68008f3b48e25325c545f71039c4a373f9bd4
Brendan Ashworth <brendan.ashworth@me.com>
2022-10-03 08:59:10 -0700

    test cache

---
+++ tests/test_cache.py
@@ -0,0 +1,12 @@
+
+from mundipy.pcs import choose_pcs
+from shapely.geometry import box
+
+def test_cache_choose():
+    bbox = (-118.843683, 34.052235, -118.143683, 34.552235)
+    polygon = box(*bbox, ccw=True)
+
+    res_first = choose_pcs(polygon)
+    res_second = choose_pcs(polygon)
+
+    assert res_first == res_second
