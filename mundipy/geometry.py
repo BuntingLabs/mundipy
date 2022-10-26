f4ae02d19b4819d9dd6a8ea1c1fb48541eb74522
Brendan Ashworth <brendan.ashworth@me.com>
2022-10-26 15:19:55 -0700

    linestring

--- mundipy/geometry.py
+++ mundipy/geometry.py
@@ -18,6 +18,19 @@ class Point(geom.Point):
 	def __setitem__(self, item, value):
 		self.features[item] = value
 
+class LineString(geom.LineString):
+
+	def __init__(self, geo: geom.LineString, features: dict):
+		super().__init__(geo)
+
+		self.features = features
+
+	def __getitem__(self, item):
+		return self.features[item]
+
+	def __setitem__(self, item, value):
+		self.features[item] = value
+
 class Polygon(geom.Polygon):
 
 	def __init__(self, geo: geom.Polygon, features: dict):
@@ -56,12 +69,14 @@ def from_geojson(geojson: dict):
 
 		if feature['geometry']['type'] == 'Point':
 			features.append(Point(shape(feature['geometry']), feature['properties']))
+		elif feature['geometry']['type'] == 'LineString':
+			features.append(LineString(shape(feature['geometry']), feature['properties']))
 		elif feature['geometry']['type'] == 'Polygon':
 			features.append(Polygon(shape(feature['geometry']), feature['properties']))
 		elif feature['geometry']['type'] == 'MultiPolygon':
 			features.append(MultiPolygon(shape(feature['geometry']), feature['properties']))
 		else:
-			raise TypeError('geometry type was %s, expected Point/Polygon/MultiPolygon' % feature['geometry']['type'])
+			raise TypeError('geometry type was %s, expected Point/LineString/Polygon/MultiPolygon' % feature['geometry']['type'])
 
 	return features
 
@@ -72,12 +87,14 @@ def from_row_series(row: pd.Series):
 
 	if isinstance(geo, geom.Point):
 		return Point(geo, features)
+	elif isinstance(geo, geom.LineString):
+		return LineString(geo, features)
 	elif isinstance(geo, geom.Polygon):
 		return Polygon(geo, features)
 	elif isinstance(geo, geom.MultiPolygon):
 		return MultiPolygon(geo, features)
 	else:
-		raise TypeError('from_row_series got %s, expected Point/Polygon/MultiPolygon' % str(type(geo)))
+		raise TypeError('from_row_series got %s, expected Point/LineString/Polygon/MultiPolygon' % str(type(geo)))
 
 def from_dataframe(gdf: gpd.GeoDataFrame):
 	return from_geojson(json.loads(gdf.to_json()))
