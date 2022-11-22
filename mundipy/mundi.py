19ac9ee6ab22fed2ef73c5724cbe283a6a031676
Brendan Ashworth <brendan.ashworth@me.com>
2022-11-22 15:52:56 -0800

    drop matplotlib plotting, geopandas dep

--- mundipy/mundi.py
+++ mundipy/mundi.py
@@ -13,9 +13,6 @@ from shapely.ops import transform
 from shapely.geometry import Polygon, MultiPolygon, LineString, Point, box
 from shapely.geometry.collection import GeometryCollection
 from shapely.geometry.base import BaseGeometry
-import matplotlib.pyplot as plt
-import matplotlib.colors as mcolors
-import matplotlib.patches as mpatches
 
 from mundipy.map import Map
 from mundipy.layer import Dataset
@@ -27,17 +24,14 @@ import mundipy.geometry as geom
 from mundipy.utils import _plot
 
 class MundiQ:
-    def __init__(self, center, mapdata, plot_target=None, units='meters'):
+    def __init__(self, center, mapdata, units='meters'):
         # a shapely object in EPSG:4326
         self.center = center
 
         # GeoPool
         self.mapdata = mapdata
 
-        # could be matplotlib axes or 'geojson'
-        self.plot_target = plot_target
-        self.plot_legend = dict()
-        self.plot_handles = []
+        # list of shapely features
         self.plot_contents = []
 
     def call_process(self, fn):
@@ -67,41 +61,26 @@ class MundiQ:
         return self._bbox(distance).bounds
 
     def plot(self, shape, name):
-        if self.plot_target is None:
+        """
+        Plot a shape in the current MundiQ context. Shape can be
+        a list of mundipy geometries or a single mundipy geometry.
+        """
+        if isinstance(shape, list):
+            for single_shape in shape:
+                self.plot(single_shape, name)
+
             return
 
-        # coerce shape into a GeoSeries, no matter what it is
-        # shape can literally be anything
-        if isinstance(shape, Point) or isinstance(shape, Polygon) or isinstance(shape, MultiPolygon) or isinstance(shape, LineString):
-            shape = gpd.GeoSeries([shape])
-        # convert from our geom first
-        elif isinstance(shape, geom.Point) or isinstance(shape, geom.Polygon) or isinstance(shape, geom.MultiPolygon) or isinstance(shape, geom.LineString):
-            shape = gpd.GeoSeries([shape._geo])
-        elif isinstance(shape, gpd.GeoDataFrame):
-            shape = shape.geometry
-        elif isinstance(shape, list):
-            shape = gpd.GeoSeries(shape)
-        else:
-            raise TypeError('unexpected type passed to plot(), got %s' % type(shape))
+        if not isinstance(shape, geom.BaseGeometry):
+            raise TypeError('mundipy.plot() requires mundipy BaseGeometry but got "%s"' % type(shape))
 
         # fix shapes
-        shape = shape.apply(lambda g: g.buffer(0) if isinstance(g, Polygon) or isinstance(g, MultiPolygon) else g)
+        shape = shape.transform('EPSG:4326')._geo
+        if isinstance(shape, Polygon) or isinstance(shape, MultiPolygon):
+            shape = shape.buffer(0)
 
-        # check color
-        if name in self.plot_legend.keys():
-            color = self.plot_legend[name]
-        else:
-            # grab next color in list
-            color = list(mcolors.CSS4_COLORS.keys())[len(self.plot_legend)]
-            self.plot_legend[name] = color
-            self.plot_handles.append(mpatches.Patch(color=color, label=name))
-
-        if self.plot_target == 'geojson':
-            # plot_contents can only contain shapely geometries
-            for idx, row in shape.items():
-                self.plot_contents.append((row, mcolors.CSS4_COLORS[color]))
-        else:
-            shape.plot(ax=self.plot_target, edgecolor='black', facecolor=color, aspect='equal', alpha=0.75)
+        # plot_contents can only contain shapely geometries
+        self.plot_contents.append(shape)
 
 class Mundi:
     def __init__(self, mapdata, main: str, units='meters'):
@@ -113,18 +92,13 @@ class Mundi:
             raise TypeError('units passed to Mundi() was neither meters nor feet')
         self.units = units
 
-    def plot(self, fn, element_index=0, output_type='matplotlib'):
-        if output_type == 'geojson':
-            pass
-        elif output_type == 'matplotlib':
-            fig, ax = plt.subplots()
-
+    def plot(self, fn, element_index=0):
         if element_index < 0 or element_index > len(self.main.geometry_collection()):
             raise TypeError('element_index passed to plot() that was < 0 or > length of dataset')
 
         # TODO: drop duplicates, except it's very slow
         #.drop_duplicates(subset=['geometry'])
-        Q = MundiQ(self.main.geometry_collection()[element_index], self.mapdata, plot_target=('geojson' if output_type == 'geojson' else ax), units=self.units)
+        Q = MundiQ(self.main.geometry_collection()[element_index], self.mapdata, units=self.units)
 
         with redirect_stdout(io.StringIO()) as f:
             res = Q.call_process(fn)
@@ -139,19 +113,14 @@ class Mundi:
         res['_stdout'] = f.getvalue()
         res['_id'] = element_index
 
-        if output_type == 'geojson':
-            # merge geometries into one
-            geom_col = GeometryCollection([r[0] for r in Q.plot_contents])
+        # merge geometries into one
+        geom_col = GeometryCollection(Q.plot_contents)
 
-            return json.dumps({
-                "type": "GeometryCollection",
-                "geometries": geom_col.__geo_interface__['geometries'],
-                "properties": { k: (int(v) if isinstance(v, int) else (float(v) if isinstance(v, float) else str(v))) for (k, v) in res.items() if not isinstance(v, BaseGeometry)}
-            })
-
-        elif output_type == 'matplotlib':
-            ax.legend(handles=Q.plot_handles, loc='upper right')
-            plt.show()
+        return json.dumps({
+            "type": "GeometryCollection",
+            "geometries": geom_col.__geo_interface__['geometries'],
+            "properties": { k: (int(v) if isinstance(v, int) else (float(v) if isinstance(v, float) else str(v))) for (k, v) in res.items() if not isinstance(v, BaseGeometry)}
+        })
 
     def q(self, fn, progressbar=False, n_start=None, n_end=None):
         # make iterator unique by geometry
