9b359a693a917313ffc20712e9a3b74d885805da
Brendan Ashworth <brendan.ashworth@me.com>
2022-11-26 15:50:31 -0800

    gracefully do .make_valid()

--- tests/test_geometry.py
+++ tests/test_geometry.py
@@ -1,5 +1,11 @@
+import pytest
 
 from mundipy.layer import Dataset
+from mundipy.geometry import enrich_geom
+import mundipy.geometry as geom
+
+from shapely.geometry import Polygon, MultiPolygon
+from shapely import from_wkt, GEOSException
 
 def test_geometry():
     df = Dataset('tests/fixtures/polygon.geojson')
@@ -22,3 +28,25 @@ def test_geometry_from_row():
 
     geom['name'] = 'new_name'
     assert geom['name'] == 'new_name'
+
+def test_multi_polygon():
+    outer = enrich_geom(Polygon([[25.273428697681624,6.026182562140747],[26.063011790518573,-7.9853706778425675],[30.13724140836942,-8.088943835912445],[30.13782361496999,6.078309828324564],[25.273428697681624,6.026182562140747]]), dict())
+    inner = enrich_geom(Polygon([[18.381743157653403,-1.3600556177262746],[18.313380419378433,-4.991685343163752],[39.81788374424539,-5.007774504721652],[39.62298421833077,-0.6253503748207265],[18.381743157653403,-1.3600556177262746]]), dict())
+
+    subbed = outer.difference(inner)
+
+    assert isinstance(subbed, geom.MultiPolygon)
+
+def test_invalid_geometry_ops():
+    foo = from_wkt('POLYGON((0 0, 0 1, 2 1, 2 2, 1 2, 1 0, 0 0))')
+    bar = from_wkt('POLYGON((0 0, 0 1, 2 1, 2 2, 1 2, 1 0, 0 0))')
+
+    with pytest.raises(GEOSException, match='TopologyException'):
+        foo.difference(bar)
+
+    mfoo = enrich_geom(foo, dict())
+    mbar = enrich_geom(bar, dict())
+
+    # no error
+    res = mfoo.difference(mbar)
+    assert res.area == 0.0
